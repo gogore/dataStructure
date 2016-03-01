161f9e7462a5f93d6e1655dc5fa5c6361576f084
higore <kjhihing@gmail.com>
2016-03-01 22:05:40 +0900

    Create diamond.js
    배열을 이용하여 다이아몬드 찍기 js

---
+++ src/algospot/diamond.js
@@ -0,0 +1,14 @@
+(function(maxWidth){
+   var space = new Array(Math.ceil(maxWidth / 2));
+   var star = "*";
+   var starInc = "**";
+   var results = [];
+   var resultsReverse = [];
+   for(; star.length <= maxWidth; star += starInc, space.pop()){
+      var line = (space.length > 0 ? space.join(" ") : "") + star;
+      results.push(line);
+      resultsReverse.unshift(line);
+   }
+   resultsReverse.shift();
+   console.log(results.join("\n") + "\n" + resultsReverse.join("\n"));
+})(14)
