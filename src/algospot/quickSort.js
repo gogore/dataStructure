87428b69ca2d9cd6a6492d1de2f73b43cd80326d
higore <kjhihing@gmail.com>
2016-04-14 16:19:09 +0900

    퀵정렬

---
+++ src/algospot/quickSort.js
@@ -0,0 +1,27 @@
+(function(array){
+  function sort(array, left, right){
+    if(left >= right) {console.log(array);return;}
+    var low, high, pivot;
+    low = left + 1, high = right;
+    pivot = array[left];
+    while(low <= high) {
+      while(low <= right && array[low] <= pivot){
+        low++;
+      }
+      while(left + 1 <= high && pivot <= array[high]){
+        high--;
+      }
+      if(low <= high){
+        var temp = array[low];
+        array[low] = array[high];
+        array[high] = temp;
+      } else {
+        array[left] = array[high];
+        array[high] = pivot;
+      }
+    }
+    sort(array, left, high-1);
+    sort(array, high + 1, right);
+  };
+  sort(array, 0, array.length -1);
+})([93,2,1,10]);
